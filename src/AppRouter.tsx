8cf90637b62bac62c3b30d475d0e7658e9ee0765
Amjath MG <amjathcool@gmail.com>
2025-05-08 00:14:35 -0400

    Removed stale code

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,14 +1,11 @@
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { swlegendGames } from "./cricket/data/swlegend-games";
 
 // Lazy load route components
 const SwLandingPage = lazy(() => import("./cricket/SwLandingPage"));
 const GameListRouterWrapper = lazy(
   () => import("./cricket/GameListRouterWrapper")
 );
-const GameGrid = lazy(() => import("./cricket/GameGrid"));
-const Tryout = lazy(() => import("./cricket/Tryout"));
 
 const Loading: React.FC = () => <div>Loading page...</div>;
 
@@ -19,15 +16,6 @@ const AppRouter: React.FC = () => {
         <Routes>
           <Route path="/" element={<SwLandingPage />} />
           <Route path="/:teamId" element={<GameListRouterWrapper />} />
-          {/* Below routes ag and try added for exploring upcoming features */}
-          <Route
-            path="/ag"
-            element={<GameGrid teamName="Amjath" games={swlegendGames} />}
-          />
-          <Route
-            path="/try"
-            element={<Tryout teamName="SW Legends" games={swlegendGames} />}
-          />
         </Routes>
       </Suspense>
     </Router>
